85204aa346fc0474abd7f70bff37a84e0d878d71
Patrick Huffer <huffer@rhywun.com>
2022-07-24 15:58:24 -0400

    Fix line endings

--- RulesEngineDemo/Demos/CalculationsDemo.cs
+++ RulesEngineDemo/Demos/CalculationsDemo.cs
@@ -9,25 +9,25 @@ namespace RulesEngineDemo.Demos;
 
 public static class CalculationsDemo
 {
-    public static async void Run()
-    {
-        Console.WriteLine("\nRunning Calculations demo...\n");
+	public static async void Run()
+	{
+		Console.WriteLine("\nRunning Calculations demo...\n");
 
-        // Set up the rules engine with workflow(s)
-        var engine = new RulesEngine.RulesEngine(new[] { Calculations.Workflow });
+		// Set up the rules engine with workflow(s)
+		var engine = new RulesEngine.RulesEngine(new[] { Calculations.Workflow });
 
-        // Execute a workflow's rules against an input
-        WriteLine("Contract1...");
-        var contract = new RuleParameter("contract", Contract1);
-        var insured = new RuleParameter("insured", new Insured { State = "NY" });
-        var results = await engine.ExecuteAllRulesAsync("Calculations", contract, insured);
+		// Execute a workflow's rules against an input
+		WriteLine("Contract1...");
+		var contract = new RuleParameter("contract", Contract1);
+		var insured = new RuleParameter("insured", new Insured { State = "NY" });
+		var results = await engine.ExecuteAllRulesAsync("Calculations", contract, insured);
 
-        // Display the results
-        ReportResults(results);
+		// Display the results
+		ReportResults(results);
 
-        WriteLine($"\n\tPremium before: {Contract1.Premium:c}");
+		WriteLine($"\n\tPremium before: {Contract1.Premium:c}");
         decimal newPremium = (decimal)results.First()?.ActionResult?.Output;
-        Contract1.Premium = newPremium;
-        WriteLine($"\tPremium after: {Contract1.Premium:c}");
-    }
+		Contract1.Premium = newPremium;
+		WriteLine($"\tPremium after: {Contract1.Premium:c}");
+	}
 }
--- RulesEngineDemo/Demos/EFDemo.cs
+++ RulesEngineDemo/Demos/EFDemo.cs
@@ -10,59 +10,59 @@ namespace RulesEngineDemo.Demos;
 
 public static class EFDemo
 {
-    private static readonly RulesEngineDemoContext _db;
+	private static readonly RulesEngineDemoContext _db;
 
-    static EFDemo()
-    {
-        // Save the workflow array to the database
-        _db = new RulesEngineDemoContext();
-        _db.Database.EnsureDeleted();
-        if (_db.Database.EnsureCreated())
-        {
+	static EFDemo()
+	{
+		// Save the workflow array to the database
+		_db = new RulesEngineDemoContext();
+		_db.Database.EnsureDeleted();
+		if (_db.Database.EnsureCreated())
+		{
             _db.Workflows.AddRange(Validations.Workflow, Calculations.Workflow);
-            _db.SaveChanges();
-        }
-    }
+			_db.SaveChanges();
+		}
+	}
 
-    public static async void Run()
-    {
-        // Initialize the rules engine with an array of workflows
+	public static async void Run()
+	{
+		// Initialize the rules engine with an array of workflows
         var workflows = _db.Workflows.Include(i => i.Rules)
-                           .ThenInclude(i => i.Rules)
-                           .ToArray();
-        var engine = new RulesEngine.RulesEngine(workflows);
-        
-        WriteLine("\nRunning validations...\n");
-        
-        WriteLine("Contract1...");
-        var contract = new RuleParameter("contract", Contract1);
-        var insured = new RuleParameter("insured", new Insured { State = "NY" });
-        var results = await engine.ExecuteAllRulesAsync("Validations", contract, insured);
-        ReportResults(results);
+		                   .ThenInclude(i => i.Rules)
+		                   .ToArray();
+		var engine = new RulesEngine.RulesEngine(workflows);
 
-        WriteLine("Contract2...");
-        contract = new RuleParameter("contract", Contract2);
-        insured = new RuleParameter("insured", new Insured { State = "CA" });
-        results = await engine.ExecuteAllRulesAsync("Validations", contract, insured);
-        ReportResults(results);
+		WriteLine("\nRunning validations...\n");
 
-        WriteLine("Contract3...");
-        contract = new RuleParameter("contract", Contract3);
-        insured = new RuleParameter("insured", new Insured { State = "CA" });
-        results = await engine.ExecuteAllRulesAsync("Validations", contract, insured);
-        ReportResults(results);
+		WriteLine("Contract1...");
+		var contract = new RuleParameter("contract", Contract1);
+		var insured = new RuleParameter("insured", new Insured { State = "NY" });
+		var results = await engine.ExecuteAllRulesAsync("Validations", contract, insured);
+		ReportResults(results);
 
-        WriteLine("\nRunning calculations...\n");
+		WriteLine("Contract2...");
+		contract = new RuleParameter("contract", Contract2);
+		insured = new RuleParameter("insured", new Insured { State = "CA" });
+		results = await engine.ExecuteAllRulesAsync("Validations", contract, insured);
+		ReportResults(results);
 
-        WriteLine("Contract1...");
-        contract = new RuleParameter("contract", Contract1);
-        insured = new RuleParameter("insured", new Insured { State = "NY" });
-        results = await engine.ExecuteAllRulesAsync("Calculations", contract, insured);
-        ReportResults(results);
+		WriteLine("Contract3...");
+		contract = new RuleParameter("contract", Contract3);
+		insured = new RuleParameter("insured", new Insured { State = "CA" });
+		results = await engine.ExecuteAllRulesAsync("Validations", contract, insured);
+		ReportResults(results);
 
-        WriteLine($"\n\tPremium before: {Contract1.Premium:c}");
+		WriteLine("\nRunning calculations...\n");
+
+		WriteLine("Contract1...");
+		contract = new RuleParameter("contract", Contract1);
+		insured = new RuleParameter("insured", new Insured { State = "NY" });
+		results = await engine.ExecuteAllRulesAsync("Calculations", contract, insured);
+		ReportResults(results);
+
+		WriteLine($"\n\tPremium before: {Contract1.Premium:c}");
         decimal newPremium = (decimal)results.First()?.ActionResult?.Output;
-        Contract1.Premium = newPremium;
-        WriteLine($"\tPremium after: {Contract1.Premium:c}");
-    }
+		Contract1.Premium = newPremium;
+		WriteLine($"\tPremium after: {Contract1.Premium:c}");
+	}
 }
