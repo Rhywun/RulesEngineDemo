1c73c37f52bb368d5d198daa22b0cd896b917f6c
Patrick Huffer <patrick.huffer@amtrustgroup.com>
2022-07-27 13:30:23 -0400

    Implemented GetResults

--- SRNetReportingRulesEngine/Helpers.cs
+++ SRNetReportingRulesEngine/Helpers.cs
@@ -1,56 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using RulesEngine.Models;
+using SRNetReportingRulesEngine.Models;
 using static System.Console;
 
 namespace SRNetReportingRulesEngine
 {
-	internal static class Helpers
+    internal static class Helpers
 	{
 		internal static List<Result> GetResults(List<RuleResultTree> results)
 		{
-			// TODO: Implement this
-			throw new NotImplementedException();
+			return results.Select(result => new Result
+			{
+				RuleName = result.Rule.RuleName,
+				IsSuccess = result.IsSuccess,
+				Message = result.IsSuccess ? result.Rule.SuccessEvent : (string.IsNullOrEmpty(result.ExceptionMessage) ? result.Rule.ErrorMessage : result.ExceptionMessage),
+				Output = result.ActionResult.Output,
+			}).ToList();
 		}
 		
 		internal static void ReportResults(List<RuleResultTree> results)
 		{
-			// Loop through each parent rule
 			results.ForEach(result =>
 			{
 				WriteLine(result.IsSuccess
-					? $"\tPass: {result.Rule.RuleName} - {result.Rule.SuccessEvent}"
-					: $"\tFail: {result.Rule.RuleName} " +
+					? $"Pass: {result.Rule.RuleName} - {result.Rule.SuccessEvent}"
+					: $"Fail: {result.Rule.RuleName} " +
 					  $"- {(string.IsNullOrEmpty(result.ExceptionMessage) ? result.Rule.ErrorMessage : result.ExceptionMessage)}");
 
 				if (result.ActionResult.Output != null)
 					WriteLine($"\tOutput: {result.ActionResult.Output}");
-
-				if (result.ChildResults is null)
-				{
-					return;
-				}
-
-				if (result.Rule.Operator == "Or")
-				{
-					WriteLine("\t\tOne of the following rules should pass:");
-				}
-				else if (result.Rule.Operator == "And")
-				{
-					WriteLine("\t\tAll of the following rules should pass:");
-				}
-
-				// Loop through each child rule
-				foreach (var childResult in result.ChildResults)
-				{
-					WriteLine(childResult.IsSuccess
-						? $"\t\tPass: {childResult.Rule.RuleName} - {childResult.Rule.SuccessEvent}"
-						: $"\t\tFail: {childResult.Rule.RuleName} " +
-						  $"- {(string.IsNullOrEmpty(childResult.ExceptionMessage) ? childResult.Rule.ErrorMessage : childResult.ExceptionMessage)}");
-
-					if (childResult.ActionResult.Output != null)
-						WriteLine($"\t\tOutput: {childResult.ActionResult.Output}");
-				}
 			});
 		}
 	}
