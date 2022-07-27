785792e0309a0e12e99bfb787f613073beae3e91
Patrick Huffer <patrick.huffer@amtrustgroup.com>
2022-07-27 13:31:24 -0400

    Refactor to allow async Main method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using ConsoleApp1.Models;
 using RulesEngine.Models;
 
@@ -6,7 +7,12 @@ namespace ConsoleApp1
 {
 	class Program
 	{
-		static void Main(string[] args)
+		public static void Main()
+		{
+			MainAsync().GetAwaiter().GetResult();
+		}
+
+		private static async Task MainAsync()
 		{
 			var contract = new Contract
 			{
@@ -18,14 +24,20 @@ namespace ConsoleApp1
 				ExpirationDate = new DateTime(2023, 1, 1),
 				TermMonths = 12,
 				ClaimLimit = 600,
-				ClaimAmount = 400,
+				ClaimAmount = 800,
 				Premium = 999,
 			};
 			
 			var engine = new SRNetReportingRulesEngine.Engine();
+
 			var param1 = new RuleParameter("contract", contract);
 			var param2 = new RuleParameter("insured", new Insured { State = "NY" });
-			engine.RunWorkflow("Validations", new[] { param1, param2 });
+
+			var results1 = await engine.RunWorkflow("Validations", new[] { param1, param2 });
+			results1.ForEach(result => Console.WriteLine(result));
+
+			var results2 = await engine.RunWorkflow("Calculations", new[] { param1, param2 });
+			results2.ForEach(result => Console.WriteLine(result));
 		}
 	}
 }
